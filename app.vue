80332beca3fb5be3fafd497300012a7341251a22
robin-whg <mail@robin-whg.com>
2023-12-24 10:47:16 +0100

    Refactor app.vue to use NuxtPage component and set html attrs

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    class: 'bg-zinc-900 py-12 text-zinc-50 font-sans',
+  },
+})
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
