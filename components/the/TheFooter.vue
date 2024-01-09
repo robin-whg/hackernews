7d5ae0c172c5eacf405f1b3fb63c77d31f5817b1
robin-whg <mail@robin-whg.com>
2024-01-09 20:41:31 +0100

    add taget _blank

--- components/the/TheFooter.vue
+++ components/the/TheFooter.vue
@@ -3,12 +3,12 @@
     <div class="mx-auto max-w-2xl flex justify-between py-6">
       <div class="text-sm text-zinc-400">
         2024
-        <NuxtLink to="https://robin-whg.com" class="hover:underline">
+        <NuxtLink to="https://robin-whg.com" target="_blank" class="hover:underline">
           robin-whg
         </NuxtLink>
       </div>
       <div>
-        <NuxtLink to="https://github.com/robin-whg/y-news" class="group flex items-center gap-1 rounded px-1.5 py-.5 text-sm text-zinc-400 transition duration-100 hover:bg-zinc-800 hover:text-white">
+        <NuxtLink target="_blank" to="https://github.com/robin-whg/y-news" class="group flex items-center gap-1 rounded px-1.5 py-.5 text-sm text-zinc-400 transition duration-100 hover:bg-zinc-800 hover:text-white">
           <div class="i-tabler-brand-github h-4 w-4 flex-none" />
           Github
         </NuxtLink>
