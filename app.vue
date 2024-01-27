71e6e398b975908bfa6a428ebcefb4fe68140d74
robin-whg <mail@robin-whg.com>
2024-01-27 23:39:59 +0100

    add page transition

--- app.vue
+++ app.vue
@@ -30,3 +30,14 @@ useHead({
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.15s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
