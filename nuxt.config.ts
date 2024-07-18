a1c5baf3912d437439d53592e6d1c7105fe241a4
robin-whg <mail@robin-whg.com>
2024-07-18 15:59:51 +0200

    build: update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
   },
+
   ssr: false,
+
   typescript: {
     typeCheck: true,
   },
+
   imports: {
     dirs: ['types'],
   },
+
   modules: ['@unocss/nuxt', '@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt', 'nuxt-headlessui', '@nuxtjs/robots', '@vueuse/nuxt'],
+
   unocss: {
     preflight: true,
   },
+
+  compatibilityDate: '2024-07-18',
 })
