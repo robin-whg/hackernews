4c72c639c42868e5f12866133b942ce7a68a610e
robin-whg <mail@robin-whg.com>
2024-07-21 18:05:45 +0200

    fix: prevent recursive updates error

--- components/the/TheFooter.vue
+++ components/the/TheFooter.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="flex justify-between p-3">
-    <div class="text-sm text-gray-700 dark:text-gray-200 flex items-center gap-1.5">
+  <div class="flex justify-between p-3 text-sm text-gray-700 dark:text-gray-200 ">
+    <div class="flex items-center gap-1.5">
       2024
       <ULink to="https://robin-whg.com" target="_blank" class="hover:underline">
         robin-whg
       </ULink>
     </div>
     <div>
-      <UButton icon="i-tabler-brand-github" variant="ghost" color="gray" size="sm" target="_blank" to="https://github.com/robin-whg/hackernews">
+      <ULink class="hover:underline" target="_blank" to="https://github.com/robin-whg/hackernews">
         Github
-      </UButton>
+      </ULink>
     </div>
   </div>
 </template>
